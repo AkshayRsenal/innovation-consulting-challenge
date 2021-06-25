d01a7534894a0d4e6c60833455739a344e646ddd
Akshay Raul <akshay.raul@stud.hochschule-heidelberg.de>
2021-06-25 17:15:58 +0200

    Landing page with scroll

--- innovation-consulting-challenge/src/App.tsx
+++ innovation-consulting-challenge/src/App.tsx
@@ -3,14 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import NavTopBar from './components/shared/NavTopBar';
 // const Router = require("react-router-dom");
-import{ BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from './components/LandingPage';
 
 function App() {
   return (
     <Router>
-      <NavTopBar/>
-        <Route path="/" exact component={LandingPage} />
+      <NavTopBar />
+      <Route path="/" exact component={LandingPage} />
     </Router>
   );
 }
--- innovation-consulting-challenge/src/components/LandingPage.tsx
+++ innovation-consulting-challenge/src/components/LandingPage.tsx
@@ -1,15 +1,139 @@
 import '../styles/css/LandingPage.component.style.css';
-
+import hero_img from '../static/images/hero_pizza.jpg'
+// import React from 'react';
+// import Zoom from 'react-reveal/Zoom';
+import React, { useLayoutEffect, useRef, useState } from "react";
+import { render } from '@testing-library/react';
 
 function LandingPage() {
+    const [addHiddenClassName, setAddHiddenClassName] = React.useState(false);
+
+    let hiddenClassName;
+    let nextScrollUP = 1400
+    let nextScrollDown = 1400;
+    let onScroll = () => {
+        const scrollPos = window.scrollY + window.innerHeight;
+        console.log(scrollPos)
+        if (scrollPos > 1500) {
+            if (scrollPos != nextScrollDown && scrollPos > 1500) {
+                hiddenClassName = "hidden-element";
+                console.log('finding')
+                setAddHiddenClassName(
+                    true
+                );
+            } else {
+                hiddenClassName = "";
+                console.log('lesses reaches')
+                setAddHiddenClassName(
+                    false
+                );
+            }
+
+            nextScrollUP = window.scrollY + window.innerHeight - 3;
+            nextScrollDown = window.scrollY + window.innerHeight + 3;
+
+        } else {
+            hiddenClassName = "";
+            console.log('notttttt reaches')
+            setAddHiddenClassName(
+                false
+            );
+        }
+
+    }
+
+    window.addEventListener("scroll", onScroll);
+
     return (
-        <div className="container-fluid margin_top">
-            <div className="d-flex justify-content-start setWidth row col-xs-6" >
-                <img src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/3c68aba2-441a-41b9-8718-2d7b00e1a208/hero_pizza.6a4f314f.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20210623%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210623T143557Z&X-Amz-Expires=86400&X-Amz-Signature=6f4da896bb1748163e1b819fe4351f2f695786d6c8fe2ea0be9bbe3bac55dec1&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22hero_pizza.6a4f314f.jpg%22" alt="Hero_Pizza" />
+
+        <div className="container-fluid">
+
+            <div className="img-class">
+                <div className="container-fluid setWidth" >
+
+                </div>
+                <div className="overlay-text" >
+                    <div className="col-md-12">
+                        <span className="large-text">Die Haftplichtversicherung für Berufseinsteiger</span>
+                    </div>
+                    <div className="vertical-adjust">
+                        <span className="medium-text">Unbeschwert das Leben genießen</span>
+                    </div>
+                    <div className="col-md-12 button-margin">
+                        <button className="btn btn-success my-2 my-sm-0 " >Meinen Preis Berechnen</button>
+                    </div>
+                </div>
             </div>
-            <div className="d-flex justify-content-start setWidth col-xs-3 col-smoffset-4" >
-                <img src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/3c68aba2-441a-41b9-8718-2d7b00e1a208/hero_pizza.6a4f314f.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20210623%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210623T143557Z&X-Amz-Expires=86400&X-Amz-Signature=6f4da896bb1748163e1b819fe4351f2f695786d6c8fe2ea0be9bbe3bac55dec1&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22hero_pizza.6a4f314f.jpg%22" alt="Hero_Pizza" />
+            <div className="clearfix"></div>
+
+            <div className="section-two clearfix">
+                <div className="container-fluid row">
+                    <div className="col-lg-8 col-md-6 col-xs-12 section-two-margin" >
+                        <div className="col-xs-12 col-sm-2 col-md-2 col-lg-2">
+                        </div>
+                        <div className="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+                            <div>
+
+                                <span className="text-xl text-navy">Du haftest? Wir zahlen.</span>
+                            </div>
+                            <div className="text-end">
+                                <span className="text-xs text-navy">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-sm-6 col-md-6 col-xs-12 d-flex section-two-margin">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</div>
+                </div>
             </div>
+            <div className="container-fluid section-three section-three-margin">
+                <div className="text-center col-xs-12 col-sm-12 col-md-12 col-lg-12" >
+                    <span className="text-xl text-navy">Schnell. Flexibel. Sicher</span>
+                </div>
+                <div className="row">
+                    <div className="col-lg-4 col-md-4 col-xs-12">
+                        <a className="navbar-brand" href="#">
+                            <img src="https://via.placeholder.com/200x200" alt="logo" title="logo_title" />
+                        </a>
+                    </div>
+                    <div className="col-lg-4 col-md-4 col-xs-12">
+                        <a className="navbar-brand" href="#">
+                            <img src="https://via.placeholder.com/200x200" alt="logo" title="logo_title" />
+                        </a>
+                    </div>
+                    <div className="col-lg-4 col-md-4 col-xs-12">
+                        <a className="navbar-brand" href="#">
+                            <img src="https://via.placeholder.com/200x200" alt="logo" title="logo_title" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="container-fluid section-four">
+                <div className="text-center" >
+                    <span className="text-xl text-navy">Kostet nicht die Welt. Aber schützt Deine</span>
+                </div>
+                <div className="row">
+                    <div className="col-lg-6 col-md-6 col-xs-12">
+                        <a className="navbar-brand" href="#">
+                            <img src="https://via.placeholder.com/200x200" alt="logo" title="logo_title" />
+                        </a>
+                    </div>
+                    <div className="col-lg-6 col-md-6 col-xs-12">
+                        <a className="navbar-brand" href="#">
+                            <img src="https://via.placeholder.com/200x200" alt="logo" title="logo_title" />
+                        </a>
+                    </div>
+
+                </div>
+
+            </div>
+            <div>
+                {(addHiddenClassName ? (<div className={"col-lg-12 col-md-12 button-margin text-center d-flex justify-content-center" + " " + hiddenClassName + addHiddenClassName}>
+                    <button className="btn btn-success my-2 my-sm-0 button-float" >Meinen Preis Berechnen</button>
+                </div>) : (<div className={"col-lg-12 col-md-12 button-margin text-center d-flex justify-content-center" + " " + hiddenClassName + addHiddenClassName}>
+
+                </div>))}
+
+            </div>
+
         </div>
     )
 }
